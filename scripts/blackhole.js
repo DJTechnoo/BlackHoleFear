731748586167dc381f22047ea564b474e2f499b4
askelej <askelej@stud.ntnu.no>
2018-09-14 20:53:22 +0200

    removed createvector

--- scripts/blackhole.js
+++ scripts/blackhole.js
@@ -1,11 +1,14 @@
-class BlackHole {
-  constructor() {
-    this.pos = createVector(0, 0);
-    this.vel = createVector(0, 0);
-  }
+function BlackHole() {
+    this.x = 0;
+	this.y = 0;
+    //this.vel = createVector(0, 0);
+  
 
-  update() {
-    this.pos.x += this.vel.x;
-    this.pos.y += this.vel.x;
+  this.update = function()  {
+   // this.pos.x += this.vel.x;
+   // this.pos.y += this.vel.x;
+	fill(color(14,14,14));
+	ellipse(this.x, this.y,13,13);
+	
   }
 }
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -2,3 +2,11 @@ function setup() {
   createCanvas(1024, 512);
   background(color("pink"));
 }
+
+var hole = new BlackHole();
+
+function draw(){
+	
+	hole.update();
+	
+}
