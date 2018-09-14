435db2310f755b725195b8228c32c2bb380477b9
askelej <askelej@stud.ntnu.no>
2018-09-14 22:11:50 +0200

    Black hole in middle, and fake growing

--- scripts/blackhole.js
+++ scripts/blackhole.js
@@ -1,11 +1,14 @@
 function BlackHole() {
-  this.pos = createVector(0, 0);
-  this.vel = createVector(0, 0);
+	this.pos = createVector(W/2, H/2);
+	this.vel = createVector(0, 0);
+	this.s = 3;
 
-  this.update = function() {
-    this.pos.x += this.vel.x;
-    this.pos.y += this.vel.y;
-    fill(color(14, 14, 14));
-    ellipse(this.pos.x, this.pos.y, 13, 13);
+	
+	this.update = function() {
+		this.pos.x += this.vel.x;
+		this.pos.y += this.vel.y;
+		this.s += 0.5;
+		fill(color(14, 14, 14));
+		ellipse(this.pos.x, this.pos.y, this.s, this.s);
   };
 }
