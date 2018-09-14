5d151dc1bc23bb95a74cfa4347577541df72bdaf
askelej <askelej@stud.ntnu.no>
2018-09-14 22:04:53 +0200

    refactored, added clear, and item

---
+++ scripts/item.js
@@ -0,0 +1,11 @@
+function Item() {
+	this.s = 10;		// size
+	this.pos = createVector(400, 400);
+	this.vel = createVector(0, 1);
+
+  this.update = function() {
+    this.pos.y += this.vel.y
+    fill(color(0, 255, 255));
+    rect(this.pos.x, this.pos.y, this.s, this.s);
+  };
+}
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,11 +1,26 @@
+const H = 512;
+const W = 1024;
+
 var hole;
+var item;
+
+function clear2(col){
+	
+	fill(color(col));
+	rect(0, 0, W, H);
+	
+}
 
 function setup() {
-  createCanvas(1024, 512);
-  background(color("pink"));
-  hole = new BlackHole();
+	pixelDensity(1);
+	createCanvas(W, H);
+	background(color("pink"));
+	hole = new BlackHole();
+	item = new Item();
 }
 
 function draw() {
-  hole.update();
+	clear2("pink");
+	hole.update();
+	item.update();
 }
